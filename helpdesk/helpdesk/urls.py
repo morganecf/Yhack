2d7d2a6723d9d99acae49b81fb31e0b0e0b0374c
jesuiswk <jesuiswk@gmail.com>
2013-11-09 13:49:00 -0500

    SDF

--- helpdesk/helpdesk/urls.py
+++ helpdesk/helpdesk/urls.py
@@ -3,7 +3,11 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+<<<<<<< HEAD
 from helpdesk.views.views import helloworld
+=======
+#from helpdesk.views.views import helloworld
+>>>>>>> 084e6ed098a387c96d1b77e4616f8b55a6d14ce1
 from helpdesk.views.sessions import signup
 
 urlpatterns = patterns('',
@@ -12,6 +16,10 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+<<<<<<< HEAD
     url(r'^home/', helloworld),
+=======
+    #url(r'^home/', helloworld),
+>>>>>>> 084e6ed098a387c96d1b77e4616f8b55a6d14ce1
     url(r'^hello/', signup),
 )
--- helpdesk/helpdesk/views/views.py
+++ helpdesk/helpdesk/views/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+#from django.shortcuts import render
 
 # TEST METHOD 
+<<<<<<< HEAD
 def helloworld(request):
 	context = {}
 	return render(request, 'helloworld.html', context)
@@ -15,3 +16,19 @@ def home(request):
 	# 	context = {"courses": }
 
 	return render(request, 'home.html', context)
+=======
+# def helloworld(request):
+# 	context = {}
+# 	return render(request, 'home.html', context)
+
+# def home(request):
+# 	context = {}
+# 	# try:
+# 	# 	memberID = request.session['member_id']
+		
+# 	# except KeyError:
+
+# 	# 	context = {"courses": }
+
+# 	return render(request, 'home.html', context)
+>>>>>>> 084e6ed098a387c96d1b77e4616f8b55a6d14ce1
